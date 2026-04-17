3048f010d967cb023f8d37e8a0981a93135ed54a
roma-dxunvrs <r.shulai@yandex.ru>
2026-04-17 16:24:43 +0300

    Init spring boot project

---
+++ src/main/java/com/dxunvrs/mailnotify_service/MailnotifyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dxunvrs.mailnotify_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailnotifyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailnotifyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxunvrs/mailnotify_service/MailnotifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxunvrs.mailnotify_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailnotifyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
